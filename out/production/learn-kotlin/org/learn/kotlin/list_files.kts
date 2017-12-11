900223432f9dfa3bffdb34b6de7f9f71c8325fb5
Batuhan <bapaydin67@gmail.com>
2017-12-11 15:44:58 +0300

    inital commit

--- out/production/learn-kotlin/org/learn/kotlin/list_files.kts
+++
@@ -1,9 +0,0 @@
-import java.io.File
-/* this class is similar example of kotlin script file */
-val folders = File(args[0]).listFiles { file ->
-    file.isDirectory()
-}
-
-folders?.forEach { folder ->
-    println(folder)
-}
